8f25134149dc6d6ef5de7b385c35cb530e19649e
acarston <amcarston@gmail.com>
2023-12-13 15:22:54 -0700

    update c++ code

--- cpp_source/TextFile.cpp
+++ cpp_source/TextFile.cpp
@@ -26,14 +26,15 @@ int TextFile::compare(WordInfo*& current, WordInfo*& incoming) {
 // so attempting to insert should yank this element and delete the created WordInfo
 void TextFile::on_duplicate(WordInfo*& current, WordInfo*& incoming) {
     if (incoming->people.front() != current->people.back()) current->people.push_back(incoming->people.front());
+    current->count++;
     delete incoming;
     incoming = nullptr;
 }
 
 // specify the output upon traversal visit
 void TextFile::to_console(WordInfo*& info) {
-    info->count = info->people.size();
-    std::cout << info->word << "(" << info->count << "): ";
+    info->numPeople = info->people.size();
+    std::cout << info->word << "(" << info->numPeople << " people, " << info->count << " occurences): ";
     for (auto it = info->people.begin(); it != info->people.end(); ++it) {
         std::cout << *it << " ";
     }
@@ -41,7 +42,7 @@ void TextFile::to_console(WordInfo*& info) {
 }
 
 void TextFile::to_file(WordInfo*& info, std::ofstream& fout) {
-    info->count = info->people.size();
+    info->numPeople = info->people.size();
     fout << info->word + ",";
 
     auto it = info->people.begin();
@@ -50,10 +51,15 @@ void TextFile::to_file(WordInfo*& info, std::ofstream& fout) {
         fout << ";" << *it;
     }
 
+    fout << "," << info->numPeople;
     fout << "," << info->count;
     fout << "\n";
 }
 
+
+bool TextFile::has_punctuation(const std::string& word) const {
+    return PUNCTUATION.find(word[word.size() - 1]) != PUNCTUATION.end();
+}
 
 void TextFile::remove_special_chars(std::string& word) const {
     while (true) {
@@ -67,9 +73,40 @@ void TextFile::insert_word(std::string& word) {
     to_lower(word);
     remove_special_chars(word);
 
+    auto word_size = word.size();
+    for (int i = 0; i < word_size; ++i) {
+        if (SPLIT_CHARS.find(word[i]) != SPLIT_CHARS.end()) {
+            std::string word1 = word.substr(0, i);
+            std::string word2 = word.substr(i + 1, word_size);
+            insert_word(word1);
+            insert_word(word2);
+            return;
+        }
+    }
+
+    if (word_size < 1) return;
+
+    if (is_number(word)) return;
     if (THROW_WORDS.find(word) != THROW_WORDS.end()) return;
 
     WordInfo* wordInfo = new WordInfo(word, this->person);
+    tree.insert(wordInfo, &compare, &on_duplicate);
+}
+
+void TextFile::insert_word(std::string& word1, std::string& word2) {
+    if (has_punctuation(word1)) return;
+
+    remove_special_chars(word1);
+    remove_special_chars(word2);
+
+    to_lower(word1);
+    to_lower(word2);
+
+    if (THROW_PHRASES.find(word1) != THROW_PHRASES.end() || word1.size() < 1) return;
+    if (THROW_PHRASES.find(word2) != THROW_PHRASES.end() || word2.size() < 1) return;
+
+    std::string phrase = "\"" + word1 + " " + word2 + "\"";
+    WordInfo* wordInfo = new WordInfo(phrase, this->person);
     tree.insert(wordInfo, &compare, &on_duplicate);
 }
 
@@ -79,11 +116,24 @@ void TextFile::set_input(const std::string& in, const std::string& person) {
     this->person = person;
 }
 
-// add each word in the file to the tree
-void TextFile::parse_into_tree() {
+// add each word or phrase in the file to the tree
+void TextFile::parse_into_tree(bool phrases) {
     std::istringstream iss(this->in);
-    std::string word;
-    while (iss >> word) insert_word(word);
+
+    if (phrases) {
+        std::string word;
+        std::string nextWord;
+
+        iss >> word;
+        while (iss >> nextWord) {
+            insert_word(word, nextWord);
+            word = nextWord;
+        }
+    }
+    else {
+        std::string word;
+        while (iss >> word) insert_word(word);
+    }
 }
 
 // output the words alphabetically
--- cpp_source/TextFile.h
+++ cpp_source/TextFile.h
@@ -19,7 +19,8 @@ private:
 	struct WordInfo {
 		std::string word;
 		std::list<std::string> people;
-		int count = 0;
+		int numPeople = 0;
+		int count = 1;
 
 		WordInfo(std::string word, std::string person) {
 			this->word = word;
@@ -31,11 +32,17 @@ private:
 	std::string in = "";
 	std::string person = "";
 	const static int NUM_SPECIAL_CHARS = 15;
-	const std::unordered_set<char> SPECIAL_CHARS{ ',', '.', '\"', '\'', '?', ';', ':', '!', '-', '(', ')', '[', ']', '\n' };
+	const std::unordered_set<char> SPLIT_CHARS{ '/', '\\', '-' };
+	const std::unordered_set<char> PUNCTUATION{ '.', ',', '?', ';', ':', '!', '-', '(', ')', '[', ']' };
+	const std::unordered_set<char> SPECIAL_CHARS{ ',', '.', '\"', '\'', '?', ';', ':', '!', '-', '(', ')', '[', ']', '\n', ' ', '&', '+' };
 	const std::unordered_set<std::string> THROW_WORDS{ "the", "be", "to", "of", "and", "a", "in", "that", "have", "i", "it",
-		"for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say",
-		"her", "she", "or", "an", "will", "my", "all", "would", "there", "their", "what", "so", "if", "about", "who", "get",
-		"which", "go", "me", "when", "make", "just", "him", "into", "your" };
+		"for", "not" "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "her", "does"
+		"she", "or", "an", "will", "my", "would", "there", "their", "what", "so", "if", "about", "who", "get", "than", "which", 
+		"go", "me", "when", "him", "into", "your", "am", "are", "been", "being", "doing", "etc", "is", "it's", "then", "our" };
+	const std::unordered_set<std::string> THROW_PHRASES{ "the", "be", "to", "of", "and", "a", "in", "that", "have", "i", "it",
+		"for", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "her", "she", 
+		"or", "an", "my", "would", "there", "their", "what", "so", "if", "about", "who", "get", "than", "which", "go", "me", 
+		"when", "him", "into", "your", "am", "are", "been", "being", "doing", "etc", "is", "it's", "then", "our", "does" };
 
 	static void to_lower(std::string& str);
 	bool is_number(const std::string& str) const;
@@ -45,15 +52,17 @@ private:
 	static void to_console(WordInfo*& info);
 	static void to_file(WordInfo*& info, std::ofstream& fout);
 
+	bool has_punctuation(const std::string& word) const;
 	void remove_special_chars(std::string& word) const;
 	void insert_word(std::string& word);
+	void insert_word(std::string& word1, std::string& word2);
 
 public:
 	TextFile() {};
 	TextFile(const std::string& in) : in(in) {};
 
 	void set_input(const std::string& in, const std::string& person);
-	void parse_into_tree();
+	void parse_into_tree(bool phrases = false);
 	void print_words();
 	void print_words(const std::string& filePath);
 	void print_input() const;
@@ -80,8 +89,8 @@ extern "C" {
 		std::string filePathStr(ws.begin(), ws.end());
 		textFile->print_words(filePathStr);
 	};
-	TEXTFILE_API void c_parse_into_tree(TextFile* textFile) {
-		textFile->parse_into_tree();
+	TEXTFILE_API void c_parse_into_tree(TextFile* textFile, bool phrases) {
+		textFile->parse_into_tree(phrases);
 	};
 }
 
